306347565014da6f1403ccef0cc13ffc88fae525
David Rosko <rosko.dav@gmail.com>
2023-09-11 17:57:55 +0200

    Add serviceTag

--- APIGateway.Heimstaden.SessionView/Startup.cs
+++ APIGateway.Heimstaden.SessionView/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Sentry;
 
 namespace APIGateway.Heimstaden.SessionView
 {
@@ -65,6 +66,10 @@ namespace APIGateway.Heimstaden.SessionView
             app.UseRouting();
 
             app.UseSentryTracing();
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.SetTag("Service", nameof(APIGateway.Heimstaden.SessionView));
+            });
 
             app.UseAuthorization();
 
